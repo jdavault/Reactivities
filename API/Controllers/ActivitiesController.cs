991b30b538cdfd24b15352045aa2cff09a4e9a02
Joe Davault <joe.davault@gmail.com>
2021-12-14 16:04:58 -0700

    Added the rest of the crud opeations

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,6 +5,7 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
 using Application.Activities;
+using System.Threading;
 
 namespace API.Controllers
 {
@@ -28,10 +29,16 @@ namespace API.Controllers
             return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
         }
         [HttpPut("{id}")]
-        public async Task<IActionResult>  EditActivity(Guid id, Activity activity)
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
             return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+        }
+
     }
 }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -6,25 +6,29 @@ using System.Threading;
 using MediatR;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>>{}
+        public class Query : IRequest<List<Activity>> { }
 
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly ILogger<List> _logger;
+            public Handler(DataContext context, ILogger<List> logger)
             {
+                _logger = logger;
                 _context = context;
 
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.ToListAsync();
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
 
